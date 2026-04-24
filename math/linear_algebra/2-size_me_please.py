9264c57b4f7dbe90545a848a1d78c9d400c6c90a
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-04-24 23:16:40 +0400

    Task

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module that calculates the shape of a matrix"""
 
+
 def matrix_shape(matrix):
     """Returns the shape of a matrix as a list of integers"""
     shape = []
