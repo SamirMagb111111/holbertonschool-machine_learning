6b9aa6ba2ed9326f54deae1b1859d44742f81add
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-04-24 23:33:46 +0400

    Task

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that performs matrix multiplication using numpy"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Returns the matrix product"""
+    return np.matmul(mat1, mat2)
