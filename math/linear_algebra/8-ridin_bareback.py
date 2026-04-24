e995b490ef8039902c537edb3642def47ab47354
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-04-24 23:25:30 +0400

    Task

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module that performs matrix multiplication"""
+
+
+def mat_mul(mat1, mat2):
+    """Returns the product of two matrices"""
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    result = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat2[0])):
+            s = 0
+            for k in range(len(mat2)):
+                s += mat1[i][k] * mat2[k][j]
+            row.append(s)
+        result.append(row)
+    return result
