4ca959f884de273c0143b6a6873145970ccf4533
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-04-24 23:15:45 +0400

    Task

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""Module that calculates the shape of a matrix"""
+
 def matrix_shape(matrix):
+    """Returns the shape of a matrix as a list of integers"""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
