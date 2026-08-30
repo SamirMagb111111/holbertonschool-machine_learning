e8b862129592e9116b36739d5f9be1086a0c7c24
Samir Mammadov <samir.memmedov.adnsu2004@gmail.com>
2026-08-30 23:55:29 +0400

    Create sparse autoencoder

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Sparse autoencoder module."""
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """Create a sparse autoencoder."""
+    encoder_input = keras.Input(shape=(input_dims,))
+    x = encoder_input
+
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(
+            nodes,
+            activation='relu'
+        )(x)
+
+    latent = keras.layers.Dense(
+        latent_dims,
+        activation='relu',
+        activity_regularizer=keras.regularizers.l1(lambtha)
+    )(x)
+
+    encoder = keras.Model(
+        inputs=encoder_input,
+        outputs=latent
+    )
+
+    decoder_input = keras.Input(shape=(latent_dims,))
+    x = decoder_input
+
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(
+            nodes,
+            activation='relu'
+        )(x)
+
+    decoder_output = keras.layers.Dense(
+        input_dims,
+        activation='sigmoid'
+    )(x)
+
+    decoder = keras.Model(
+        inputs=decoder_input,
+        outputs=decoder_output
+    )
+
+    auto_output = decoder(encoder(encoder_input))
+
+    auto = keras.Model(
+        inputs=encoder_input,
+        outputs=auto_output
+    )
+
+    auto.compile(
+        optimizer='adam',
+        loss='binary_crossentropy'
+    )
+
+    return encoder, decoder, auto
